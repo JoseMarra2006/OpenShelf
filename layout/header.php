587d5e9dd30de50a953988867675241e3b193a68
José Ernesto Marra Filho <JoseErnesto>
2025-11-03 16:18:00 -0300

    bug in header fixed

--- layout/header.php
+++ layout/header.php
@@ -79,35 +79,3 @@
         </div>
         <?php unset($_SESSION['success']); ?>
     <?php endif; ?>
-<?php
-
-    $logged = $_SESSION["logged"] ?? "false";
-    $user = $_SESSION["username"] ?? null;
-    $role = $_SESSION["role"] ?? "user";
-
-    if($logged == "true") :
-?>
-
-    <h3>You are already logged.</h3>
-    <p>Click <a href="/main-page">here</a> to get back to the home page</Strong></p>
-
-<?php else : ?>
-
-<section class="register-login-user">
-    <h2 class="book-home">Login on OpenShelf</h2>
-
-    <form id="register-login-form" method="POST" action="/login/save">
-        <div class="user-email">
-            <p class="question">E-mail</p>
-            <input type="email" name="user-email" placeholder="E-mail" required>
-        </div>
-        <div class="user-password">
-            <p class="question">Password</p>
-            <input type="password" name="user-password" placeholder="Password" required> 
-            </div>
-        <div>
-            <button class="btn-login" type="submit">Login</button>
-        </div>
-    </form>
-</section>
-<?php endif; ?>
